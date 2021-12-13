3c064e29e4306b6ed1d32c5d4f2a1cf9714997a8
aymyo <hello.aymyo@gmail.com>
2021-12-13 17:53:48 +0100

    footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "@fontsource/inria-sans"; // Defaults to weight 400 with normal variant.
+import "@fontsource/inria-serif"; // Defaults to weight 400 with normal variant.
+import "@fontsource/inria-serif/700-italic.css"; // Defaults to weight 400 with normal variant.
+import "@fontsource/inria-serif/400-italic.css"; // Defaults to weight 400 with normal variant.
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: 'var(--light)',
+        dark: 'var(--dark)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        tertiary: 'var(--tertiary)',
+        accent: 'var(--accent)',
+      },
+      fontFamily: {
+        'sans': ["Inria Sans","ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        'serif': ["Inria Serif", 'ui-serif', "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
+      },
+      backgroundImage: {
+        'tesselation-3-pattern': "url('/img/tesselation-3.svg')",
+        'footer-pattern': "url('/img/footer.svg')",
+      },
+      listStyleType: {
+         circle: 'circle'
+      },      
+      boxShadow: {
+        'btn': '2px 4px rgba(0, 0, 0, 0.25)',
+      }
+    }
   },
-  plugins: [],
-}
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
